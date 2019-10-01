1c1817d20527ae6ef7e0ba1cd311c1ecfe0aa6e9
alexeygree <alexnogreen@gmail.com>
2019-10-01 11:33:19 +0300

    Задание 3. Исправлены имена переменных, удалены лишние циклы из функции generatePins

--- js/main.js
+++ js/main.js
@@ -3,73 +3,82 @@
 var map = document.querySelector('.map');
 var mapPins = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-var mockData = {
-  AVATAR: ['01', '02', '03', '04', '05', '06', '07', '08'],
+var offerParams = {
   TITLE: ['Квартира 1', 'Квартира 2', 'Квартира 3', 'Квартира 4'],
-  ADDRESS: 'x, y',
   PRICE: [500, 700, 900, 1100, 1300],
   TYPE: ['palace', 'flat', 'house', 'bungalo'],
   ROOMS: [1, 2, 3, 4],
   GUESTS: [0, 1, 2, 3, 4, 5, 6],
-  CHECKIN: ['12:00', '13:00', '14:00'],
-  CHECKOUT: ['12:00', '13:00', '14:00'],
+  TIMES: ['12:00', '13:00', '14:00'],
   FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
   DESCRIPTION: ['Описание 1', 'Описание 2', 'Описание 3', 'Описание 4'],
-  PHOTOS: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
-  Y_MIN: 130,
-  Y_MAX: 630
+  PHOTOS: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
 };
+var yCord = {
+  MIN: 130,
+  MAX: 630
+};
+var numberOfRooms = {
+  MIN: 1,
+  MAX: 4
+};
+var numberofGuests = {
+  MIN: 0,
+  MAX: 10
+};
+var PINS_AMOUNT = 8;
 
 var getRandomElement = function (arr) {
   return arr[Math.round(Math.random() * (arr.length - 1))];
 };
 
-var getRandomElementInRange = function (min, max) {
+var getRandomNumberInRange = function (min, max) {
   return Math.floor(min + Math.random() * (max - min));
 };
 
-var getRandomArray = function (arr) {
+var getRandomElements = function (arr) {
   var random = Math.round(Math.random() * (arr.length - 1));
-  var finalArray = [];
+  var values = [];
   for (var i = 0; i < random; i++) {
-    finalArray.push(arr[random]);
+    values.push(arr[random]);
   }
-  return finalArray;
+  return values;
 };
 
-var generatePins = function (pinsAmount) {
-  var arr = [];
+var generatePins = function (amount) {
+  var pins = [];
   // var photoCounter = []; Другой вариант проверки уже выбранной аватарки
-  for (var i = 0; i < pinsAmount; i++) {
-    var photoNum = getRandomElement(mockData.AVATAR);
+  for (var i = 0; i < amount; i++) {
+    // var avatarNumber = '0' + (i + 1);
+
     // photoCounter.push(photoNum); Другой вариант проверки уже выбранной аватарки
-    for (var j = 0; j < mockData.AVATAR.length; j++) {
-      if (photoNum === undefined) {
-        photoNum = '01';
-      }
-      if (photoNum === mockData.AVATAR[j]) {
-        mockData.AVATAR.splice(j, 1);
-      }
-    }
-    var x = getRandomElementInRange(0, mapPins.clientWidth);
-    var y = getRandomElementInRange(mockData.Y_MIN, mockData.Y_MAX);
+    // for (var j = 0; j < PINS_AMOUNT; j++) {
+    //   if (photoNum === undefined) {
+    //     photoNum = '01';
+    //   }
+    //   if (photoNum === offerParams.AVATAR[j]) {
+    //     offerParams.AVATAR.splice(j, 1);
+    //   }
+    // }
+    var x = getRandomNumberInRange(0, mapPins.clientWidth);
+    var y = getRandomNumberInRange(yCord.MIN, yCord.MAX);
     var pin = {
       'author': {
-        'avatar': 'img/avatars/user' + photoNum + '.png'
+        'avatar': 'img/avatars/user0' + (i + 1) + '.png'
       },
 
       'offer': {
-        'title': getRandomElement(mockData.TITLE),
+        'title': getRandomElement(offerParams.TITLE),
         'address': x + ', ' + y,
-        'price': getRandomElement(mockData.PRICE),
-        'type': getRandomElement(mockData.TYPE),
-        'rooms': getRandomElement(mockData.ROOMS),
-        'guests': getRandomElement(mockData.GUESTS),
-        'checkin': getRandomElement(mockData.CHECKIN),
-        'checkout': getRandomElement(mockData.CHECKOUT),
-        'features': getRandomArray(mockData.FEATURES),
-        'description': getRandomElement(mockData.DESCRIPTION),
-        'photos': getRandomArray(mockData.PHOTOS)
+        'price': getRandomElement(offerParams.PRICE),
+        'type': getRandomElement(offerParams.TYPE),
+        'rooms': getRandomNumberInRange(numberOfRooms.MIN, numberOfRooms.MAX),
+        'guests': getRandomNumberInRange(numberofGuests.MIN, numberofGuests.MAX),
+        'checkin': getRandomElement(offerParams.TIMES),
+        'checkout': getRandomElement(offerParams.TIMES),
+        'features': getRandomElements(offerParams.FEATURES),
+        'description': getRandomElement(offerParams.DESCRIPTION),
+        'photos': getRandomElements(offerParams.PHOTOS)
       },
 
       'location': {
@@ -77,27 +86,29 @@ var generatePins = function (pinsAmount) {
         'y': y
       }
     };
-    arr.push(pin);
+    pins.push(pin);
   }
-  return arr;
+  return pins;
 };
 
-var PINS_AMOUNT = 8;
 var pins = generatePins(PINS_AMOUNT);
 
 var renderPin = function (pin) {
   var currentPin = pinTemplate.cloneNode(true);
+  var image = currentPin.querySelector('img');
 
-  currentPin.setAttribute('style', 'left: ' + pin.location.x + 'px; top: ' + pin.location.y + 'px; transform: translateX(-25px) translateY(-70px);');
-  currentPin.querySelector('img').setAttribute('src', pin.author.avatar);
-  currentPin.querySelector('img').setAttribute('alt', pin.offer.title);
+  currentPin.style.left = pin.location.x + 'px';
+  currentPin.style.top = pin.location.y + 'px';
+  currentPin.style.transform = 'translateX(-25px) translateY(-70px)';
+  image.src = pin.author.avatar;
+  image.alt = pin.offer.title;
 
   return currentPin;
 };
 
-var addPins = function (pinsArr) {
+var addPins = function (pinsCollection) {
   var fragment = document.createDocumentFragment();
-  pinsArr.forEach(function (pin) {
+  pinsCollection.forEach(function (pin) {
     fragment.appendChild(renderPin(pin));
   });
 
